0f9a4406897b76600a8c2ee70c1e229af31dff8c
phanthanhloc3107 <ptloc2438@clc.fitus.edu.vn>
2025-12-08 23:06:37 +0700

    Mới nhất

--- SHOPPY_Supabase/static/js/index.js
+++ SHOPPY_Supabase/static/js/index.js
@@ -259,7 +259,7 @@ function renderSearchResults(products, searchQuery = '') {
 	// Nếu không có sản phẩm
 	if (!products || products.length === 0) {
 		wrap.innerHTML = `
-			<div class="no-results" style="text-align:center; padding:40px 20px; grid-column:1/-1; color:#666;">
+			<div class="no-results" style="text-align:center; padding:40px 20px; width:100%; color:#666;">
 				<svg xmlns="http://www.w3.org/2000/svg" height="60" viewBox="0 -960 960 960" width="60" fill="#ccc" style="margin-bottom:20px;">
 					<path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/>
 				</svg>
